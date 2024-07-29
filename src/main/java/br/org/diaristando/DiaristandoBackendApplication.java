54574cf2cf6567d4ca1cf984c69d69fc67a572c0
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2024-07-29 20:03:44 -0300

    base project

---
+++ src/main/java/br/org/diaristando/DiaristandoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.org.diaristando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaristandoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaristandoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/diaristando/DiaristandoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.diaristando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaristandoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
